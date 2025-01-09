e8309baf3b8be77544f208e25dc7620cdd164db6
zhangl32 <lu.zhang@lixil.com>
2025-01-09 15:19:25 +0900

    commit

---
+++ src/app/app.functions/call-webhook-endpoint.js
@@ -0,0 +1,34 @@
+// For HubSpot API calls
+const axios = require('axios');
+const hubspot = require('@hubspot/api-client');
+
+exports.main = async (context = {}) => {
+  const hubspotClient = new hubspot.Client({
+    accessToken: process.env['PRIVATE_APP_ACCESS_TOKEN'],
+  });
+  
+  // DealId
+  const { dealId } = context.parameters;
+
+  try {
+      await callWebhookEndpoint(dealId);
+      return { status: 'success', meesage:"Workflow initiated successfully. Please wait for 4-5 minutes while the system completes the processing.  Thank you for your patience!" };
+  } catch (err) {
+    return { status: 'error', message: err.message }
+  }
+};
+
+
+const callWebhookEndpoint = async (dealId) => {
+  return axios.post(
+    `https://api-na1.hubapi.com/automation/v4/webhook-triggers/39703267/Fgue9nS`,
+    {
+        dealId: dealId,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+};
